9aa41f9ea69260c8ec27d5efee3b4d0dff56764e
Alex Gonzalez <alexgonzalez1124@gmail.com>
2015-09-18 11:05:29 -0500

    Se Suben los tres proyectos sin nada de contenido

---
+++ Maquila_Controller/src/main/java/com/mycompany/maquila_controller/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.maquila_controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Maquila_Entities/src/main/java/com/mycompany/maquila_entities/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.maquila_entities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
