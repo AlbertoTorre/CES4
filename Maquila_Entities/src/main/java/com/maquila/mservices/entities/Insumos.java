ce28abc49ecbcd13ed1194b37ca5c06715328f34
diego <diego8525@gmail.com>
2015-09-19 20:43:41 -0500

    Archvios generado en clase

---
+++ Maquila_Entities/src/main/java/com/maquila/mservices/entities/Insumos.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.maquila.mservices.entities;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Insumos {
+    
+}
